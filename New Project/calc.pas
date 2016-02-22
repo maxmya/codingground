31ab8cb47e70a8e69184ca6df17f3fde3371c989
Mahmoud Aref <maxmya@outlook.com>
2016-02-22 18:34:16 +0200

    Create calc.pas

---
+++ New Project/calc.pas
@@ -0,0 +1,20 @@
+program factorial;
+ 
+function fact(n: integer): longint;
+
+begin
+    if (n = 0) then
+        fact := 1
+    else
+        fact := n * fact(n -1);
+end;
+ 
+var
+    n: integer;
+ 
+begin
+    	
+    for n := 0 to 16 do
+	
+        writeln(n, '! = ', fact(n));
+end
