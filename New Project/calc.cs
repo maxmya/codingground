8e0b9da36397080846155f0109d66401860c6396
FawzyMmdouh <shaktora@gmail.com>
2016-02-23 00:14:04 +0200

    Update calc.cs

--- New Project/calc.cs
+++ New Project/calc.cs
@@ -18,8 +18,8 @@ namespace Calc
 				double num1 = Convert.ToDouble (stringnum1);
 				Console.Write ("enter num2 ");
 				string stringnum2 = Console.ReadLine ();
-			    double num2 = Convert.ToDouble (stringnum2);
-			    switch(op){
+			    	double num2 = Convert.ToDouble (stringnum2);
+			    	switch(op){
 				case 1:
 					Console.Write ((num1 +" + " + num2 +" = " )+ (num1 + num2)+"\n");
 					break;
@@ -30,7 +30,7 @@ namespace Calc
 					Console.Write ((num1 +" * " + num2 +" = " )+ (num1 * num2)+"\n");
 					break;
 				case 4:{
-				    if (num2 != 0)
+				    	if (num2 != 0)
 					Console.Write ((num1 +" / " + num2 +" = " )+ (num1 / num2)+"\n");
 					else
 					Console.Write("cannot devide by 0 !\n");
