c98d32857f46b7dabb32f532ef4a422259d4dcdd
Ahmad18750 <Ahmad.18750@yahoo.com>
2016-02-22 23:32:38 +0200

    Update calc.java

--- New Project/calc.java
+++ New Project/calc.java
@@ -3,7 +3,7 @@ public class cal {
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
         System.out.println("Enter two integers and operation");
-        System.out.println("Operations are + - * / and ^");
+        System.out.println("Operations are + - * / ^ and !");
         System.out.println("Enter 0 as operation to end program ");
         int x,y;
         String s;
@@ -12,22 +12,38 @@ public class cal {
             y=in.nextInt();
             s=in.next();
             if(s.equals("0"))System.exit(0);
-            if(!s.equals("^"))
-                System.out.println("Answer is "+op(x,y,s));
-            else 
-                System.out.println("Answer is "+pow(x,y));
+            System.out.println(op(x,y,s));
         }
+    }
+    static int fct(int x){
+        if(x<1){
+            System.out.println("input must be grater than 1");
+            return -1;
+        }
+        if(x==1)return x;
+        return x*fct(x-1);
     }
     static double op(int x,int y,String o){
         switch (o){
             case "+":return x+y;
             case "-":return x-y;
             case "*":return x*y;
-            case "/":return x/y;
+            case "/":if(y==0){
+                        System.out.println("can't devide by zero");
+                        return -1;
+                    }
+                    else return x/y;
+            case "!":return fct(x);
+            case "^":return pow(x,y);
+            default :System.out.println("operation not supported");
         }
-        return 0;
+        return -1;
     }
     static int pow(int x,int y){
+        if(y<0){
+            System.out.println("negative power not supported");
+            return -1;
+        }
         int ans=x;
         for(int i=1;i<y;i++)
             ans*=x;
