0fcdee3b679067543567d0ab7bcbc9608ae8a94b
Ahmad18750 <Ahmad.18750@yahoo.com>
2016-02-22 23:13:18 +0200

    Update calc.go

--- New Project/calc.go
+++ New Project/calc.go
@@ -1,29 +1,36 @@
-    package main
-    import "fmt"
-     
-    func main(){
-    	fmt.Println("Enter two integers and operation")
-    	fmt.Println("1-addition 2-substaction 3-multiplication 4-devision 5-raise to power") 
-    	fmt.Println("type 0 as an operation to end program")
-    	var a, b, s int
-    	fmt.Scanf("%d %d %d", &a , &b , &s)
-    	for {
-    		switch s {
-    			case 1 :fmt.Println(a + b)
-    			case 2 :fmt.Println(a - b)
-    			case 3 :fmt.Println(a * b)
-    			case 4 :fmt.Println(a / b)
-    			case 5 :fmt.Println(pow(a , b))
-    		}
-    		fmt.Scanf("%d %d %d", &a , &b , &s)
-    		if s == 0 {break}
-    	}
-    }
-    func pow(a int, b int)int {
-    	var c int =1
-     
-    	for i := 1; i<=b; i++ {
-    		c*=a;
-    	}
-    	return c;
-    }
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("Enter two integers and operation")
+	fmt.Println("1-addition 2-substaction 3-multiplication 4-devision 5-raise to power ")
+	fmt.Println("type 0 as an operation to end program")
+	var a, b, s int
+	fmt.Scanf("%d %d %d", &a , &b , &s)
+	for {
+		switch s {
+			case 1 :fmt.Println(a + b)
+			case 2 :fmt.Println(a - b)
+			case 3 :fmt.Println(a * b)
+			case 4 :if b == 0 {
+						fmt.Println("Can't devide by zero !")
+					} else {
+						fmt.Println(a / b)
+					}
+			case 5 :if b < 0 {
+						fmt.Println("negative power not supported")
+					} else {
+						fmt.Println(pow(a , b))
+					}
+		}
+		fmt.Scanf("%d %d %d", &a , &b , &s)
+		if s == 0 {break}
+	}
+}
+func pow(a int, b int)int {
+	var c int =1
+	for i := 1; i<=b; i++ {
+		c*=a;
+	}
+	return c;
+}
