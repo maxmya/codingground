e57248889a6481b05f0e1640bd1e248e65097c42
FawzyMmdouh <shaktora@gmail.com>
2016-02-23 00:13:20 +0200

    Update calc.cs

--- New Project/calc.cs
+++ New Project/calc.cs
@@ -13,26 +13,37 @@ namespace Calc
 				int op = Convert.ToInt32(stringop); 
 				if (op == 6)
 					break;
-				Console.Write ("enter num1  ");
+				Console.Write ("enter num1 ");
 				string stringnum1 = Console.ReadLine ();
 				double num1 = Convert.ToDouble (stringnum1);
-				Console.Write ("enter num2  ");
+				Console.Write ("enter num2 ");
 				string stringnum2 = Console.ReadLine ();
 			    double num2 = Convert.ToDouble (stringnum2);
-				if (op == 1)
+			    switch(op){
+				case 1:
 					Console.Write ((num1 +" + " + num2 +" = " )+ (num1 + num2)+"\n");
-				if(op == 2)
+					break;
+				case 2:
 					Console.Write ((num1 +" - " + num2 +" = " )+ (num1 - num2)+"\n");
-				if(op == 3)
+					break;
+				case 3:
 					Console.Write ((num1 +" * " + num2 +" = " )+ (num1 * num2)+"\n");
-				if(op == 4)
+					break;
+				case 4:{
+				    if (num2 != 0)
 					Console.Write ((num1 +" / " + num2 +" = " )+ (num1 / num2)+"\n");
-				if(op == 5){
+					else
+					Console.Write("cannot devide by 0 !\n");
+					break;
+				    }
+				case 5:{
 					double ans = 1;
 					for (double i = 0; i < num2; i++)
 						ans *= num1;
 					Console.Write ((num1 +" ^ " + num2 +" = " )+ (ans)+"\n");
+					break;
 					}
+			    }
 			}
 		}
 	}
